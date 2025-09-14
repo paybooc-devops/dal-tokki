23024469337217e237239b0f7464a5cb45917e20
유진세 <jinseyou@gmail.com>
2025-09-14 12:24:53 +0900

    경로 수정

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -6,10 +6,9 @@ mkdir -p data/image/origin
 mkdir -p data/image/new
 mkdir -p data/image/transparent
 mkdir -p data/video/result
-mkdir -p env
 
 # 이미지 빌드 및 태깅
 docker build -t dal-tokki-api:latest -f Dockerfile .
 
 # 스웜 배포 (Compose 파일 경로 주의: 대소문자 일치)
-docker stack deploy -c Docker-compose.yaml dal-tokki
+docker stack deploy -c docker-compose.yaml dal-tokki
