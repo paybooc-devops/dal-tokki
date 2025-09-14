d3c65f86bc6d553757cf57890d25dac13bf89656
유진세 <jinseyou@gmail.com>
2025-09-14 12:22:09 +0900

    폴더 생성

--- deploy-stack.sh
+++ deploy-stack.sh
@@ -2,8 +2,14 @@
 
 set -euo pipefail
 
+mkdir -p data/image/origin
+mkdir -p data/image/new
+mkdir -p data/image/transparent
+mkdir -p data/video/result
+mkdir -p env
+
 # 이미지 빌드 및 태깅
-docker build -t tokki-video-jobs-api:latest -f Dockerfile .
+docker build -t dal-tokki-api:latest -f Dockerfile .
 
 # 스웜 배포 (Compose 파일 경로 주의: 대소문자 일치)
 docker stack deploy -c Docker-compose.yaml dal-tokki
